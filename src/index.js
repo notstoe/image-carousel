aa8c0fd5940519e82d2fb885ef2b1874e6086b5a
Gustavo <gstonin95@gmail.com>
2020-08-22 23:46:53 +0100

    slideCtrl Buttons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import { slideRight, slideLeft } from "./scripts/passSlide";
+import { goToSlide } from "./scripts/goToSlide";
 
 const leftBtn = document.querySelector("#leftBtn");
 const rightBtn = document.querySelector("#rightBtn");
+
+const slideBtns = document.querySelectorAll(".slideBtns");
+slideBtns.forEach((slideBtn) => {
+	slideBtn.addEventListener("click", (e) => {
+		const ref = e.target.id.replace("img", "");
+		goToSlide(ref*(-70));
+	});
+});
 
 leftBtn.addEventListener("click", slideLeft);
 rightBtn.addEventListener("click", slideRight);
